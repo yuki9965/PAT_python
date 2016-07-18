a1876cfbe7675ecb2362b5a38853a7fb49c7d10a
Yaicky <ajirencnty@gmail.com>
2016-07-18 14:25:47 +0800

    2016-7-18

---
+++ baishupaidui.py
@@ -0,0 +1,24 @@
+__author__ = 'Yaicky'
+
+
+class Rat:
+    def __init__(self, weight, color):
+        self.weight = weight
+        self.color = color
+    def __cmp__(self, other):
+        return -cmp(self.weight, other.weight)
+
+
+while True:
+    try:
+        n = input()
+        rats = []
+        for i in range(n):
+            weight, color = raw_input().strip().split()
+            rats.append(Rat(int(weight),color))
+
+        rats.sort()
+        for c in rats:
+            print c.color
+    except:
+        break
---
+++ jiajianchengchu.py
@@ -0,0 +1,19 @@
+__author__ = 'Yaicky'
+
+def add(num1_1, num1_2, num2_1, num2_2):
+    down = num1_2 * num2_2
+
+
+
+
+
+
+while True:
+    try:
+        n = map(int, raw_input().strip().split(' ', '/'))
+        num1_1 = n[0]
+        num1_2 = n[1]
+        num2_1 = n[2]
+        num2_2 = n[3]
+    except:
+        break
